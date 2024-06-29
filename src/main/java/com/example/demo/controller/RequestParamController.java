92c9dbd6cc58aebc5fe8866027d9211d4af95de0
Seunghyun-Jeong <dkskakdgka@gmail.com>
2024-06-29 22:19:04 +0900

    feat: form 클래스 추가

--- src/main/java/com/example/demo/controller/RequestParamController.java
+++ src/main/java/com/example/demo/controller/RequestParamController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import com.example.demo.form.Form;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,17 +18,22 @@ public class RequestParamController {
         return "entry";
     }
 
-    /** 확인 화면을 표시 */
+//    /** 확인 화면을 표시 */
+//    @PostMapping("confirm")
+//    public String confirmView(Model model, @RequestParam String name, @RequestParam Integer age, @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @RequestParam
+//    LocalDate birth) {
+//        // Model에 저장
+//        model.addAttribute("name", name);
+//        model.addAttribute("age", age);
+//        model.addAttribute("birth", birth);
+//
+//        // 반환값으로 뷰 이름을 돌려줌
+//        return "confirm";
+//    }
+
     @PostMapping("confirm")
-    public String confirmView(Model model, @RequestParam String name, @RequestParam Integer age, @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @RequestParam
-    LocalDate birth) {
-        // Model에 저장
-        model.addAttribute("name", name);
-        model.addAttribute("age", age);
-        model.addAttribute("birth", birth);
-
+    public String confirmView(Form f) {
         // 반환값으로 뷰 이름을 돌려줌
-        return "confirm";
+        return "confirm2";
     }
-
 }
